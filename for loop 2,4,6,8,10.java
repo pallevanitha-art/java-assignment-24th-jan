44c980dec99dabe9623456d27c4473c4cfb03903
pallevanitha-art <98254318+pallevanitha-art@users.noreply.github.com>
2022-01-25 02:40:43 -0800

    Add files via upload

---
+++ for loop 2,4,6,8,10.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	   for(int  i=2; i<=10; i=i+2){
+		System.out.println(i);
+		}
+	}
+}
